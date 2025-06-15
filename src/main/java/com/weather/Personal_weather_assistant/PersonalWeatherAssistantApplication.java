505fce4789b94b80b1ecfc0e22fd00eb3ed195eb
noor188 <noormmlk@gmail.com>
2025-06-15 16:14:14 -0600

    chore: initialize Spring Boot project

---
+++ src/main/java/com/weather/Personal_weather_assistant/PersonalWeatherAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.weather.Personal_weather_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWeatherAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWeatherAssistantApplication.class, args);
+	}
+
+}
